ccfb5f39bb430af4d83013453a52f742be69adac
dobutamine <tim.antonius@outlook.com>
2021-04-19 15:44:45 +0200

    bug fix

--- public/explain_engine/components/ECG.js
+++ public/explain_engine/components/ECG.js
@@ -157,7 +157,7 @@ class ECG {
     if (this.heart_rate > 0) {
       this._sa_node_period = 60 / this.heart_rate;
     } else {
-      this._sa_node_period = 60;
+      this._sa_node_period = 600000;
     }
 
     if (this.venticular_escape_rate > 0) {
