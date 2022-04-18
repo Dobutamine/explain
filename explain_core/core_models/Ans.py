3b9042661686db9c5f1615cff5d849fc439dd0c9
dobutamine <tim.antonius@outlook.com>
2022-04-18 14:08:38 +0200

    update ans

--- explain_core/core_models/Ans.py
+++ explain_core/core_models/Ans.py
@@ -11,9 +11,8 @@ class Ans:
 
         # define the properties
         self.is_enabled = True
-        self.sensors = []
         self.effect_sites = []
-        self.ans_inputs = {}
+        self.effectors = {}
 
         # set the independent properties
         for key, value in args.items():
@@ -28,13 +27,31 @@ class Ans:
 
     def initialize(self):
         # get reference to all the sensors
-        for sensor in self.sensors:
-            self.ans_inputs[sensor["name"]] = {
-                "model": self.model.components[sensor["name"]]
+        for effect_site in self.effect_sites:
+            (model, prop) = effect_site["target"].split(".")
+            self.effectors[effect_site["name"]] = {
+                "sensor": self.model.components[effect_site["sensor"]],
+                "model": self.model.components[model],
+                "prop": prop,
+                "gain": effect_site["gain"],
+                "ref": effect_site["reference_value"]
+                
             }
+        print(self.effectors)
         self._initialized = True
 
 
     def ans_activity(self):
-       if not self._initialized:
+        if not self._initialized:
            self.initialize()
+
+        # calculate effect on heartrate
+        for (_, effector_site) in self.effectors.items():
+            # get the sensor value
+            sensor_value = (effector_site["sensor"].sensor_output - 50.0)
+            # get the gain and reference
+            gain = effector_site["gain"]
+            ref = effector_site["ref"]
+            # apply the effect
+            new_value = ref + sensor_value * gain
+            setattr(effector_site["model"], effector_site["prop"], new_value)
