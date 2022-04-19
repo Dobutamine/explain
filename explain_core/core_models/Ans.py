a408a3cc8c491881c1b3cc0cbe4ebba0277cdd72
dobutamine <tim.antonius@outlook.com>
2022-04-19 16:33:16 +0200

    removed ans functionality

--- explain_core/core_models/Ans.py
+++ explain_core/core_models/Ans.py
@@ -26,17 +26,6 @@ class Ans:
             self.ans_activity()
 
     def initialize(self):
-        # get reference to all the sensors
-        for effect_site in self.effect_sites:
-            (model, prop) = effect_site["target"].split(".")
-            self.effectors[effect_site["name"]] = {
-                "sensor": self.model.components[effect_site["sensor"]],
-                "model": self.model.components[model],
-                "prop": prop,
-                "gain": effect_site["gain"],
-                "ref": effect_site["reference_value"],
-                "prev_prop_change": 0.0
-            }
         self._initialized = True
 
 
@@ -44,24 +33,3 @@ class Ans:
         if not self._initialized:
            self.initialize()
 
-        # calculate effect on heartrate
-        for (_, effector_site) in self.effectors.items():
-            # get the sensor value
-            sensor_value = (effector_site["sensor"].sensor_output - 50.0)
-
-            # calculate the property change
-            prop_change = sensor_value * effector_site["gain"]
-
-            # calculate the new property value
-            new_value = effector_site["ref"] + prop_change
-
-            # preserve mass if the property to change is the unstressed volume
-            if effector_site["prop"] == "u_vol":
-                # change th volume as we shift volume from the u_vol to the vol
-                effector_site["model"].vol -= (prop_change - effector_site["prev_prop_change"])
-                
-                # store the current volume change
-                effector_site["prev_prop_change"] = prop_change
-
-            # set the actual property
-            setattr(effector_site["model"], effector_site["prop"], new_value)
